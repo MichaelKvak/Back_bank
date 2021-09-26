1b6843d8042f09f7fe016cf24af5e9fe678b1f74
MichaelKvak <mkvak1987@gmail.com>
2021-09-26 14:07:33 +0300

    end off

--- routes/bank.js
+++ routes/bank.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
 
 mongoose.connect("mongodb://localhost:27017/bank_db", {
   useNewUrlParser: true,
@@ -19,31 +20,73 @@ const banksScheme = new Schema({
 const Bank = mongoose.model("Bank", banksScheme);
 
 router.get("/", function (req, res, next) {
-  Bank.find({}, function (err, docs) {
-    if (err)
-      return res
-        .status(500)
-        .json({ success: false, err: { msg: "Fetch failed!" } });
-    res.status(200).json({ success: true, data: docs });
-  });
+  if (req.query.filter_property)
+    Bank.find(
+      {
+        [req.query.filter_property]: req.query.filter_value,
+      },
+      function (err, docs) {
+        if (err)
+          return res
+            .status(500)
+            .json({ success: false, err: { msg: "Fetch failed!" } });
+        res.status(200).json({ success: true, data: docs });
+      }
+    );
+  else
+    Bank.find({}, function (err, docs) {
+      if (err)
+        return res
+          .status(500)
+          .json({ success: false, err: { msg: "Fetch failed!" } });
+      res.status(200).json({ success: true, data: docs });
+    });
 });
 
-router.post("/add", function (req, res, next) {
-  const bank = new Bank({
-    name: req.body.bankName,
-    rate: parseInt(req.body.bankRate),
-    credit: parseInt(req.body.bankCredit),
-    payment: parseInt(req.body.bankPayment),
-    term: parseInt(req.body.bankTerm),
-  });
-  bank.save(function (err, bankDoc) {
-    if (err)
-      return res
-        .status(500)
-        .json({ success: false, err: { msg: "Saving failed!" } });
-    res.status(200).json({ success: true, bankId: bankDoc._id });
-  });
-});
+router.post(
+  "/add",
+  body("name")
+    .isLength({ min: 3 })
+    .trim()
+    .withMessage("Bank name must be specified")
+    .escape(),
+  body("rate")
+    .isInt({ min: 1 })
+    .withMessage("Interest rate must be  under 0.0001")
+    .toInt(),
+  body("credit")
+    .isInt({ min: 1 })
+    .withMessage("Maximum loan must be  under 1")
+    .toInt()
+    .escape(),
+  body("payment")
+    .isInt({ min: 1 })
+    .withMessage("Minimum Down Payment must be  under 1")
+    .toInt()
+    .escape(),
+  body("term")
+    .isInt({ min: 1 })
+    .withMessage("Loan Term must be  under 1")
+    .toInt()
+    .escape(),
+
+  function (req, res, next) {
+    const bank = new Bank({
+      name: req.body.bankName,
+      rate: parseFloat(req.body.bankRate),
+      credit: parseInt(req.body.bankCredit),
+      payment: parseInt(req.body.bankPayment),
+      term: parseInt(req.body.bankTerm),
+    });
+    bank.save(function (err, bankDoc) {
+      if (err)
+        return res
+          .status(500)
+          .json({ success: false, err: { msg: "Saving failed!" } });
+      res.status(200).json({ success: true, bankId: bankDoc._id });
+    });
+  }
+);
 
 router.delete("/", function (req, res, next) {
   Bank.findByIdAndDelete(req.body.bankId, function (err, doc) {
@@ -65,24 +108,51 @@ router.get("/:bankId", function (req, res, next) {
   });
 });
 
-router.put("/update", function (req, res, next) {
-  Bank.findByIdAndUpdate(
-    req.body.bankId,
-    {
-      name: req.body.bankName,
-      rate: parseInt(req.body.bankRate),
-      credit: parseInt(req.body.bankCredit),
-      payment: parseInt(req.body.bankPayment),
-      term: parseInt(req.body.bankTerm),
-    },
-    function (err, bankDoc) {
-      if (err)
-        return res
-          .status(500)
-          .json({ success: false, err: { msg: "Saving failed!" } });
-      res.status(200).json({ success: true });
-    }
-  );
-});
+router.put(
+  "/update",
+  body("name")
+    .isLength({ min: 3 })
+    .trim()
+    .withMessage("Bank name must be specified")
+    .escape(),
+  body("rate")
+    .isInt({ min: 0.0001 })
+    .withMessage("Interest rate must be  under 0.0001")
+    .toInt(),
+  body("credit")
+    .isInt({ min: 1 })
+    .withMessage("Maximum loan must be  under 1")
+    .toInt()
+    .escape(),
+  body("payment")
+    .isInt({ min: 1 })
+    .withMessage("Minimum Down Payment must be  under 1")
+    .toInt()
+    .escape(),
+  body("term")
+    .isInt({ min: 1 })
+    .withMessage("Loan Term must be  under 1")
+    .toInt()
+    .escape(),
+  function (req, res, next) {
+    Bank.findByIdAndUpdate(
+      req.body.bankId,
+      {
+        name: req.body.bankName,
+        rate: parseFloat(req.body.bankRate),
+        credit: parseInt(req.body.bankCredit),
+        payment: parseInt(req.body.bankPayment),
+        term: parseInt(req.body.bankTerm),
+      },
+      function (err, bankDoc) {
+        if (err)
+          return res
+            .status(500)
+            .json({ success: false, err: { msg: "Saving failed!" } });
+        res.status(200).json({ success: true });
+      }
+    );
+  }
+);
 
 module.exports = router;
